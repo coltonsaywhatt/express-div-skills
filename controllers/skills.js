2a3634152c6314ca30d7071e3b222d3c28a74d79
coltonsaywhatt <coltonsaywhatt@gmail.com>
2022-04-14 20:13:05 -0400

    updated controllers/skills.js

---
+++ controllers/skills.js
@@ -0,0 +1,50 @@
+const Skill = require('../models/skill');
+
+
+module.exports = {
+    index,
+    show,
+    new: newSkill,
+    create,
+    delete: deleteSkill,
+    edit,
+    update
+};
+
+
+function update(req, res) {
+    req.body.done = !!req.body.done;
+    Skill.update(req.params.id, req.body);
+    res.redirect(`/skills/${req.params.id}`);
+}
+
+function edit(req, res) {
+    const skill = Skill.getOne(req.params.id);
+    res.render('skills/edit', { skill });
+  }
+
+function deleteSkill(req, res) {
+    Skill.deleteOne(req.params.id);
+    res.redirect('/skills');
+}
+
+function create(req, res) {
+    Skill.create(req.body);
+    res.redirect('/skills');
+}
+
+function newSkill(req, res) {
+    res.render('skills/new');
+
+}
+
+function show(req, res) {
+    // Access all route params using req.parsms (object)
+    const skill = Skill.getOne(req.params.id);
+    res.render('skills/show', { skill });
+}
+
+function index(req, res) {
+    const skills = Skill.getAll();
+    res.render('skills/index', { skills });
+}
