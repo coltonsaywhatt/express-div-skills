cd116f99df64e01e5c3bc45c5ca7c6e93219d6a9
coltonsaywhatt <coltonsaywhatt@gmail.com>
2022-04-14 20:27:15 -0400

    add edit, index, new, show.ejs

--- models/skill.js
+++ models/skill.js
@@ -1,20 +1,42 @@
 const skills = [
-    {id: 12345, skill: 'Average', done: true},
-    {id: 12346, skill: 'Super', done: true},
-    {id: 12347, skill: 'Basic', done: true},
-    {id: 12348, skill: 'Superior', done: true}
-]
+    {id: 125223, skill: 'HTML', comfy: true},
+    {id: 127904, skill: 'CSS', comfy: false},
+    {id: 139608, skill: 'JavaScript', comfy: true},
+    {id: 902101, skill: 'Express', comfy: false},
+    {id: 420696, skill: 'Node', comfy: true},
+];  
 
 module.exports = {
     getAll,
-    getOne
-};
+    getOne,
+    create,
+    deleteOne, 
+    update
+}; 
+
+function update(id, skill) {
+    id = parseInt(id);
+    const updatedSkill = skills.find((skill) => skill.id === id);
+    Object.assign(updatedSkill, skill);
+}
+
+function deleteOne(id) {
+    id = parseInt(id);
+    const idx = skills.findIndex((skill) => skill.id === id);
+    skills.splice(idx, 1);
+}
+
+function create(skill) {
+    skill.id = Date.now() % 100000;
+    skill.done = false;
+    skills.push(skill);
+}
 
 function getOne(id) {
     id = parseInt(id);
-    return skills.find((skill) => skill.id === id);
-};
+    return skills.find(skill => skill.id === id);
+}
 
 function getAll() {
     return skills;
-};
+}
